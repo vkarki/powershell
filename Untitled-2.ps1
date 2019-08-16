b09abb10785225e860471c7a27386b174a0721fd
Vishal.Karki <vishal.karki@gmail.com>
2019-08-16 10:29:40 +0530

    some random stuff

---
+++ Untitled-2.ps1
@@ -0,0 +1,5 @@
+$hash = @{ Admin = 'admin123'; User1 = 'user123'; User2 = "user345"}
+
+Write-Host "LIBRARY MANAGEMENT SYSTEM"
+$uName = Read-Host -prompt "Enter UserName:"
+$uPassword = Read-Host -prompt "Enter Password:"
